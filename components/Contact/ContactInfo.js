5ae158e2a30b702c8bc2c81fe8169ab731833a4a
Max McKone <max.mckone@gmail.com>
2022-10-20 14:31:02 +0200

    contact and about us

--- components/Contact/ContactInfo.js
+++ components/Contact/ContactInfo.js
@@ -13,10 +13,7 @@ const ContactInfo = () => {
                 </div>
                 <h3>Mail Here</h3>
                 <p>
-                  <a href="mailto:admin@pungent.com">admin@pungent.com</a>
-                </p>
-                <p>
-                  <a href="mailto:info@pungent.com">info@pungent.com</a>
+                  <a href="mailto:team@adeptly.com">team@adeptly.com</a>
                 </p>
               </div>
             </div>
@@ -28,7 +25,7 @@ const ContactInfo = () => {
                 </div>
                 <h3>Visit Here</h3>
                 <p>
-                  27 Division St, New York, <br /> NY 10002, USA
+                  Gruenbergerstr. 55, 10245, Berlin
                 </p>
               </div>
             </div>
@@ -40,10 +37,7 @@ const ContactInfo = () => {
                 </div>
                 <h3>Call Here</h3>
                 <p>
-                  <a href="tel:+1234567890">+123 456 7890</a>
-                </p>
-                <p>
-                  <a href="tel:+2414524526">+241 452 4526</a>
+                  <a href="tel:017661654771">0176 6165 4771</a>
                 </p>
               </div>
             </div>
--- pages/about.js
+++ pages/about.js
@@ -2,11 +2,6 @@ import React from "react";
 import NavbarTwo from "../components/Layouts/NavbarTwo";
 import PageBanner from "../components/Common/PageBanner";
 import AboutContent from "../components/About/AboutContent";
-import FunFactsTwo from "../components/Common/FunFactsTwo";
-import TeamStyleThree from "../components/Common/TeamStyleThree";
-import Feedback from "../components/Common/Feedback";
-import Partner from "../components/Common/Partner";
-import CtaAreaTwo from "../components/Common/CtaAreaTwo";
 import Footer from "../components/Layouts/Footer";
 
 const Aabout = () => {
@@ -14,19 +9,9 @@ const Aabout = () => {
     <>
       <NavbarTwo />
 
-      <PageBanner pageTitle="About Us" BGImage="/images/page-banner1.jpg" />
+      <PageBanner pageTitle="About Us"/>
 
       <AboutContent />
-
-      <FunFactsTwo />
-
-      <TeamStyleThree />
-
-      <Feedback />
-
-      <Partner />
-
-      <CtaAreaTwo />
 
       <Footer />
     </>
